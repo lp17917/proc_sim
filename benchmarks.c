81b2faeed2881a6eb2ec15a426c04febd213c488
lp17917 <lp17917@bristol.ac.uk>
2021-03-03 18:39:28 +0000

    Added loading values to memory

--- benchmarks.c
+++ benchmarks.c
@@ -1,5 +1,25 @@
+void vector_addition_set_reg_store{
+  int i = 0
+  int a[10] = {10,5,2,2,50,7,1,99,52,0};
+  int b[10] = {15,1,100,100,10,13,0,1,132,0};
+  /*000*/INSTR_opcode[i] = LOAD_VALUE; INSTR_operandres[i] = 10; INSTR_operand1[i] = 100; INSTR_operand2[i] = 0; i++;
+  /*001*/INSTR_opcode[i] = LOAD_VALUE; INSTR_operandres[i] = 11; INSTR_operand1[i] = 150; INSTR_operand2[i] = 0; i++;
+  /*002*/INSTR_opcode[i] = LOAD_VALUE; INSTR_operandres[i] = 12; INSTR_operand1[i] = 200; INSTR_operand2[i] = 0; i++;
+  for (int r = 0; r < 10; r++){
+    /*003*/INSTR_opcode[i] = STORE_VALUE; INSTR_operandres[i] = 10; INSTR_operand1[i] = 10; INSTR_operand2[i] = 0; i++;
+    /*004*/INSTR_opcode[i] = STORE_VALUE; INSTR_operandres[i] = 11; INSTR_operand1[i] = 15; INSTR_operand2[i] = 0; i++;
+    /*005*/INSTR_opcode[i] = LOAD_VALUE; INSTR_operandres[i] = 0; INSTR_operand1[i] = 0; INSTR_operand2[i] = 1; i++; //set counter to counter + 1
+  }
+/*031*/
+  printf("%d", i)
+}
+
+
+
 int main(){
   int INSTR_opcode[512];
+  int INSTR_operandres[512];
   int INSTR_operand1[512];
   int INSTR_operand2[512];
+  vector_addition_set_reg_store();
 }
