3a9cced65be964f6734b5d50725faa5317d30707
lp17917 <lp17917@bristol.ac.uk>
2021-03-03 19:52:54 +0000

    Added loop for loading data to memory and intitalised instuction arrays

--- benchmarks.c
+++ benchmarks.c
@@ -1,17 +1,23 @@
-void vector_addition_set_reg_store{
-  int i = 0
+#include <stdio.h>
+#define LOAD_VALUE 1;
+#define STORE_VALUE 2;
+
+
+void vector_addition_set_reg_store(int *INSTR_opcode, int *INSTR_operandres, int *INSTR_operand1, int *INSTR_operand2){
+  int i = 0;
   int a[10] = {10,5,2,2,50,7,1,99,52,0};
   int b[10] = {15,1,100,100,10,13,0,1,132,0};
   /*000*/INSTR_opcode[i] = LOAD_VALUE; INSTR_operandres[i] = 10; INSTR_operand1[i] = 100; INSTR_operand2[i] = 0; i++;
   /*001*/INSTR_opcode[i] = LOAD_VALUE; INSTR_operandres[i] = 11; INSTR_operand1[i] = 150; INSTR_operand2[i] = 0; i++;
   /*002*/INSTR_opcode[i] = LOAD_VALUE; INSTR_operandres[i] = 12; INSTR_operand1[i] = 200; INSTR_operand2[i] = 0; i++;
   for (int r = 0; r < 10; r++){
-    /*003*/INSTR_opcode[i] = STORE_VALUE; INSTR_operandres[i] = 10; INSTR_operand1[i] = 10; INSTR_operand2[i] = 0; i++;
-    /*004*/INSTR_opcode[i] = STORE_VALUE; INSTR_operandres[i] = 11; INSTR_operand1[i] = 15; INSTR_operand2[i] = 0; i++;
+    /*003*/INSTR_opcode[i] = STORE_VALUE; INSTR_operandres[i] = 10; INSTR_operand1[i] = a[r]; INSTR_operand2[i] = 0; i++;
+    /*004*/INSTR_opcode[i] = STORE_VALUE; INSTR_operandres[i] = 11; INSTR_operand1[i] = b[r]; INSTR_operand2[i] = 0; i++;
     /*005*/INSTR_opcode[i] = LOAD_VALUE; INSTR_operandres[i] = 0; INSTR_operand1[i] = 0; INSTR_operand2[i] = 1; i++; //set counter to counter + 1
   }
-/*031*/
-  printf("%d", i)
+  /*033*/INSTR_opcode[i] = LOAD_VALUE; INSTR_operandres[i] = 0; INSTR_operand1[i] = 0; INSTR_operand2[i] = 0; i++;
+  printf("%d\n", i);
+
 }
 
 
@@ -21,5 +27,16 @@ int main(){
   int INSTR_operandres[512];
   int INSTR_operand1[512];
   int INSTR_operand2[512];
-  vector_addition_set_reg_store();
+  for (int i=0; i<512; i++){
+    INSTR_opcode[i] = 0;
+    INSTR_operandres[i] = 0;
+    INSTR_operand1[i] = 0;
+    INSTR_operand2[i] = 0;
+  }
+  vector_addition_set_reg_store(INSTR_opcode, INSTR_operandres, INSTR_operand1, INSTR_operand2);
+  for (int i=0; i<40; i++){
+      printf("%d: Opcode: %d Opres: %d Oprand1: %d Oprand2: %d\n", i, INSTR_opcode[i], INSTR_operandres[i], INSTR_operand1[i], INSTR_operand2[i]);
+  }
+
+
 }
