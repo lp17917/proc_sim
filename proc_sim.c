26d9798e39eddf32fdf59893fa91ee234ff33c4a
lp17917 <lp17917@bristol.ac.uk>
2021-05-04 17:34:43 +0100

    Added fetch_unit to proc_sim

--- proc_sim.c
+++ proc_sim.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "benchmarks.h"
+#include "fetch_unit.h"
 
 #define ADD 0
 #define ADD_I 1
@@ -28,6 +29,7 @@
 
 #define HALT 500
 
+/*
 int Fetch(int *opcode, int *operandres, int *operand1, int *operand2, struct INSTRUCTIONS *instr_set, int PC)
 {
   *opcode = instr_set->INSTR_opcode[PC];
@@ -36,12 +38,14 @@ int Fetch(int *opcode, int *operandres, int *operand1, int *operand2, struct INS
   *operand2 = instr_set->INSTR_operand2[PC];
   return 0;
 }
+*/
+
 
 int Decode(){
   return 0;
 }
 
-int Execute(int opcode, int r, int s1, int s2, int *RF, int *MEM, int *PC, int *ec, int *finished)
+int Execute(int opcode, int r, int s1, int s2, int *RF, int *MEM, int *ec, struct FETCH_UNIT *fetcher, int *finished)
 {
 	int error = 0;
 	switch(opcode)
@@ -50,57 +54,57 @@ int Execute(int opcode, int r, int s1, int s2, int *RF, int *MEM, int *PC, int *
 
     //Arithmatic operations
 		case ADD:
-			RF[r] = RF[s1] + RF[s2]; (*PC)++; (*ec)+=1; break;
+			RF[r] = RF[s1] + RF[s2]; (*ec)+=1; break;
     case ADD_I:
-      RF[r] = RF[s1] + s2; (*PC)++; (*ec)+=1; break;
+      RF[r] = RF[s1] + s2; (*ec)+=1; break;
 		case MUL:
-			RF[r] = RF[s1] * RF[s2]; (*PC)++; (*ec)+=2; break;
+			RF[r] = RF[s1] * RF[s2]; (*ec)+=2; break;
     case CMP:
-      if (RF[s1] == RF[s2]){ RF[r] = 0; (*PC)++; (*ec)+=1; break;}
-      else if (RF[s1] < RF[s2]){ RF[r] = -1; (*PC)++; (*ec)+=1; break;}
-      else {RF[r] = 1; (*PC)++; (*ec)+=1; break;}
+      if (RF[s1] == RF[s2]){ RF[r] = 0; (*ec)+=1; break;}
+      else if (RF[s1] < RF[s2]){ RF[r] = -1; (*ec)+=1; break;}
+      else {RF[r] = 1; (*ec)+=1; break;}
 
     //Bitwise operations
     case AND:
-      RF[r] = RF[s1] & RF[s2]; (*PC)++; (*ec)+=1; break;
+      RF[r] = RF[s1] & RF[s2]; (*ec)+=1; break;
     case OR:
-      RF[r] = RF[s1] | RF[s2]; (*PC)++; (*ec)+=1; break;
+      RF[r] = RF[s1] | RF[s2]; (*ec)+=1; break;
     case L_SHIFT:
-      RF[r] = RF[s1] << RF[s2]; (*PC)++; (*ec)+=1; break;
+      RF[r] = RF[s1] << RF[s2]; (*ec)+=1; break;
     case R_SHIFT:
-      RF[r] = RF[s1] >> RF[s2]; (*PC)++; (*ec)+=1; break;
+      RF[r] = RF[s1] >> RF[s2]; (*ec)+=1; break;
     case NOT:
-      RF[r] = ~RF[s1]; (*PC)++; (*ec)+=1; break;
+      RF[r] = ~RF[s1]; (*ec)+=1; break;
 
     //Load/store operations
 		case LOAD:
-			RF[r] = MEM[ RF[s1] + RF[s2] ]; (*PC)++; (*ec)+=3; break;
+			RF[r] = MEM[ RF[s1] + RF[s2] ]; (*ec)+=3; break;
     case LOAD_VALUE:
-      RF[r] = s1; (*PC)++; break;
+      RF[r] = s1;  break;
 		case STORE:
-			MEM[ RF[s1] + RF[s2] ] = RF[r]; (*PC)++; (*ec)+=3; break;
+			MEM[ RF[s1] + RF[s2] ] = RF[r]; (*ec)+=3; break;
     case STORE_VALUE:
-      MEM[ RF[s1] + RF[s2] ] = r; (*PC)++;  (*ec)+=3; break;
+      MEM[ RF[s1] + RF[s2] ] = r;  (*ec)+=3; break;
 
     //Branchs and jumps
 		case BRANCH_LT:
-			if (RF[s1] < RF[s2]){ *PC = r; (*ec)+=1; break;}
-      else {(*PC)++; break;}
+			if (RF[s1] < RF[s2]){ branch_pc(fetcher, r); (*ec)+=1; break;}
+      else {break;}
 		case BRANCH_NOT_ZERO:
-			if (RF[s1] != 0) {*PC = r;  (*ec)+=1; break;}
-      else {(*PC)++; break;}
+			if (RF[s1] != 0) {branch_pc(fetcher, r);  (*ec)+=1; break;}
+      else {break;}
 		case ABS_JUMP:
-			*PC = r;  (*ec)+=1; break;
+			branch_pc(fetcher, r);  (*ec)+=1; break;
     case REL_JUMP:
-        *PC += s1;  (*ec)+=1; break;
+      branch_pc(fetcher, (fetcher->PC) + s1);  (*ec)+=1; break;
 
     //Print statements
     case PRINT_INT:
-      printf("%d", RF[r]); (*PC)++;  (*ec)+=2; break;
+      printf("%d", RF[r]);  (*ec)+=2; break;
     case PRINT_CHAR_REG:
-      printf("%c",(unsigned char)RF[r] & 0xFF); (*PC)++; (*ec)+=2; break;
+      printf("%c",(unsigned char)RF[r] & 0xFF); (*ec)+=2; break;
     case PRINT_CHAR:
-      printf("%c",(unsigned char)r & 0xFF); (*PC)++;  (*ec)+=2; break;
+      printf("%c",(unsigned char)r & 0xFF);  (*ec)+=2; break;
 
 		case HALT:
 			*finished = 1; (*ec)+=1; break;
@@ -111,10 +115,11 @@ int Execute(int opcode, int r, int s1, int s2, int *RF, int *MEM, int *PC, int *
 }
 
 void run_instr_set(struct INSTRUCTIONS *instr_set){
+  struct FETCH_UNIT fetch_u;
+  load_instructs(&fetch_u, instr_set->INSTR_opcode, instr_set->INSTR_operandres, instr_set->INSTR_operand1, instr_set->INSTR_operand2);
   int finished = 0;
   int cycles = 0;
   int instructions = 0;
-  int PC = 0;
 
   int RF[32] = {0};
   int MEM[1024] = {0};
@@ -126,11 +131,12 @@ void run_instr_set(struct INSTRUCTIONS *instr_set){
     int operand1;
     int operand2;
     int executioncycles = 0;
-		Fetch(&opcode, &operandres, &operand1, &operand2, instr_set, PC);
+		Fetch(&opcode, &operandres, &operand1, &operand2, &fetch_u);
+    increment_pc(&fetch_u);
     cycles += 1;
 		Decode();
     cycles += 1;
-		Execute(opcode, operandres, operand1, operand2, RF, MEM, &PC, &executioncycles, &finished);
+		Execute(opcode, operandres, operand1, operand2, RF, MEM, &executioncycles, &fetch_u, &finished);
     cycles += executioncycles;
 		instructions++;
 
@@ -140,6 +146,7 @@ void run_instr_set(struct INSTRUCTIONS *instr_set){
 }
 
 int main() {
+
 
   struct INSTRUCTIONS instruction_set;
   generate(1, &instruction_set);
