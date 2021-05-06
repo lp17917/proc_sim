57f6cd1d7e415e5f09f83c348d138dc9287bd052
lp17917 <lp17917@bristol.ac.uk>
2021-05-06 19:23:12 +0100

    Finished single pipelining, added writeback and output forwarding

--- proc_sim.c
+++ proc_sim.c
@@ -3,11 +3,102 @@
 #include "fetch_unit.h"
 #include "types.h"
 #include <unistd.h>
+#include "ALU.h"
+#include "memory.h"
+#include "branch_unit.h"
 
+
+int check_forwarded(int *backloc, int *backval, int loc){
+  int j = -1;
+  for (int i = 0; i <32; i++){
+    if (backloc[i] == loc){
+      j = backval[i];
+    }
+  }
+
+  return j;
+}
+
+
+void load_instr(struct A_LOGIC *alu, struct MEM_U *memo, struct BRANCH_U *branchunit, struct instruction *instr, int *RF, int *backloc, int *backval){
+  struct instruction loader;
+  int fvalr = -1;
+  int fval1 = -1;
+  int fval2 = -1;
+  fval1 = check_forwarded(backloc, backval, instr->operand1);
+  fval2 = check_forwarded(backloc, backval, instr->operand2);
+  fvalr = check_forwarded(backloc, backval, instr->operandres);
+  if (instr->opcode < 200){
+    switch(instr->opcode)
+    {
+      case ADD_I:
+       loader.operand2 = instr->operand2; break;
+      default:
+        if (fval2 == -1){ loader.operand2 = RF[instr->operand2]; }
+        else{ loader.operand2 = fval2; }
+      break;
+    }
+    loader.opcode = instr->opcode;
+    loader.operandres = instr->operandres;
+
+    if (fval1 == -1){ loader.operand1 = RF[instr->operand1]; }
+    else{ loader.operand1 = fval1; }
+
+    load_instruct_alu(alu, &loader);
+  }
+
+
+  else if(instr->opcode >= 200 && instr->opcode < 300){
+    switch(instr->opcode)
+    {
+
+      case LOAD_VALUE:
+        loader.operandres = instr->operandres; loader.operand1 = instr->operand1; break;
+
+      case STORE:
+        if (fvalr == -1){ loader.operandres = RF[instr->operandres]; }
+        else{ loader.operandres = fvalr; }
+
+        if (fval1 == -1){ loader.operand1 = RF[instr->operand1]; }
+        else{ loader.operand1 = fval1; }
+        break;
+
+
+      default:
+        loader.operandres = instr->operandres;
+        if (fval1 == -1){ loader.operand1 = RF[instr->operand1]; }
+        else{ loader.operand1 = fval1; }
+        break;
+
+    }
+
+    loader.opcode = instr->opcode;
+
+    if (fval2 == -1){ loader.operand2 = RF[instr->operand2];}
+    else{loader.operand2 = fval2;}
+
+    load_instruct_memory(memo, &loader);
+  }
+
+  else if(instr->opcode >= 300 && instr->opcode < 400){
+    loader.opcode = instr->opcode;
+    loader.operandres = instr->operandres;
+
+    if (fval1 == -1){ loader.operand1 = RF[instr->operand1]; }
+    else{ loader.operand1 = fval1; }
+
+    if (fval2 == -1){ loader.operand2 = RF[instr->operand2];}
+    else{loader.operand2 = fval2;}
+    load_instruct_branch(branchunit, &loader);
+  }
+
+}
 
 int Decode(){
   return 0;
 }
+
+
 
 void print_instr(struct instruction *instr){
   printf("executing instruction: %d r: %d S1:%d S2:%d\n",instr->opcode,instr->operandres,instr->operand1,instr->operand2);
@@ -51,131 +142,156 @@ void null_instr(struct instruction *instr){
   instr->operand2 = 0;
 }
 
-int Execute(int opcode, int r, int s1, int s2, int *RF, int *MEM, struct FETCH_UNIT *fetcher, int *finished)
+int Execute(struct instruction *instr, int *RF, int *finished, struct A_LOGIC *alu, struct MEM_U *memo, struct BRANCH_U *branchunit, int *backloc, int *backval)
 {
 	int error = 0;
-	switch(opcode)
-	{
+  if (instr->opcode < 400){
+    load_instr(alu, memo, branchunit, instr, RF, backloc, backval);
+  }
 
-
-    //Arithmatic operations
-		case ADD:
-			RF[r] = RF[s1] + RF[s2]; break;
-    case ADD_I:
-      RF[r] = RF[s1] + s2; break;
-		case MUL:
-			RF[r] = RF[s1] * RF[s2]; break;
-    case CMP:
-      if (RF[s1] == RF[s2]){ RF[r] = 0; break;}
-      else if (RF[s1] < RF[s2]){ RF[r] = -1; break;}
-      else {RF[r] = 1; break;}
-
-    //Bitwise operations
-    case AND:
-      RF[r] = RF[s1] & RF[s2]; break;
-    case OR:
-      RF[r] = RF[s1] | RF[s2]; break;
-    case L_SHIFT:
-      RF[r] = RF[s1] << RF[s2]; break;
-    case R_SHIFT:
-      RF[r] = RF[s1] >> RF[s2]; break;
-    case NOT:
-      RF[r] = ~RF[s1]; break;
-
-    //Load/store operations
-		case LOAD:
-			RF[r] = MEM[ RF[s1] + RF[s2] ]; break;
-    case LOAD_VALUE:
-      RF[r] = s1;  break;
-		case STORE:
-			MEM[ RF[s1] + RF[s2] ] = RF[r]; break;
-    case STORE_VALUE:
-      MEM[ RF[s1] + RF[s2] ] = r; break;
-
-    //Branchs and jumps
-		case BRANCH_LT:
-			if (RF[s1] < RF[s2]){ branch_pc(fetcher, r); break;}
-      else {branch_pc(fetcher, (fetcher->PC) + 1); break;}
-		case BRANCH_NOT_ZERO:
-			if (RF[s1] != 0) {branch_pc(fetcher, r); break;}
-      else {branch_pc(fetcher, (fetcher->PC) + 1); break;}
-		case ABS_JUMP:
-			branch_pc(fetcher, r); break;
-
-    //Print statements
+	else{
+    switch(instr->opcode){
     case PRINT_INT:
-      printf("%d", RF[r]); break;
+      printf("%d", RF[instr->operandres]); break;
     case PRINT_CHAR_REG:
-      printf("%c",(unsigned char)RF[r] & 0xFF); break;
+      printf("%c",(unsigned char)RF[instr->operandres] & 0xFF); break;
     case PRINT_CHAR:
-      printf("%c",(unsigned char)r & 0xFF); break;
+      printf("%c",(unsigned char)instr->operandres & 0xFF); break;
 
     case NOOP:
       break;
-		case HALT:
-			*finished = 1; break;
-		default:
-			printf("Error: Opcode not recognised: %d", opcode); error = 1; break;
-	}
+    case HALT:
+      *finished = 1; break;
+    default:
+      printf("Error: Opcode not recognised: %d", instr->opcode); error = 1; break;
+
+    }
+  }
+    //Print statements
+
 	return error;
 }
 
+int writeback(int *backloc, int *backval, int *RF){
+  for (int i = 0; i < 32; i++){
+    if (backloc[i] != -1)
+      //printf("writing: %d to location: %d\n", backval[i] , backloc[i]);
+      RF[backloc[i]] = backval[i];
+      backloc[i] = -1;
+      backval[i] = 0;
+  }
+}
+
+void add_writebuffer(int *backloc, int *backval, int res, int loc){
+  int i = 0;
+  while (backloc[i] != -1){
+    i++;
+  }
+  backloc[i] = loc;
+  backval[i] = res;
+}
+
+
 void run_instr_set(struct INSTRUCTIONS *instr_set){
   struct FETCH_UNIT fetch_u;
+  struct A_LOGIC alu;
+  struct MEM_U memo;
+  struct BRANCH_U branchunit;
   struct instruction fetch_current;
   struct instruction decode_next;
   struct instruction decode_current;
   struct instruction execute_next;
+  init_a(&alu);
+  init_b(&branchunit);
+  init_m(&memo);
   null_instr(&fetch_current);
   null_instr(&decode_next);
   null_instr(&decode_current);
   null_instr(&execute_next);
-  load_instructs(&fetch_u, instr_set->INSTR_opcode, instr_set->INSTR_operandres, instr_set->INSTR_operand1, instr_set->INSTR_operand2);
+  load_instructs_fetch(&fetch_u, instr_set->INSTR_opcode, instr_set->INSTR_operandres, instr_set->INSTR_operand1, instr_set->INSTR_operand2);
   printf("set reloaded\n");
   int finished = 0;
   int cycles = 0;
   int instructions = 0;
   int RF[32] = {0};
-  int MEM[1024] = {0};
-
+  int WBloc[32];
+  for (int i = 0; i < 32; i++){
+    WBloc[i] = -1;
+  }
+  int WBval[32] = {0};
   while (!finished) {
-    //printf("%d    %d     %d\n",PC, RF[0],RF[1]);
-    int opcode;
-    int operandres;
-    int operand1;
-    int operand2;
+    writeback(WBloc, WBval, RF);
+    //printf("cycling branch\n");
+    perform_cycle_branch_u(&branchunit);
+    //printf("cycling alu\n");
+    perform_cycle_alu(&alu);
+    //printf("cycling memo\n");
+    perform_cycle_memory(&memo);
+    //printf("checking if branch writback\n");
+    if (is_finished_branch(&branchunit)){
+      if (is_jumping(&branchunit) == 0){
+        branch_pc(&fetch_u, fetch_u.PC + 1);
+      }
+      else if (is_jumping(&branchunit)  == 1){
+        branch_pc(&fetch_u, get_location(&branchunit));
+      }
 
-    if (!get_waiting(&fetch_u) && !get_halt(&fetch_u)){
-      instructions++;
-		  Fetch(&fetch_current, &fetch_u);
     }
-    else{
-      null_instr(&fetch_current);
+    //printf("checking if alu writback\n");
+    if (is_finished(&alu)){
+      if (get_out_reg(&alu) >= 0){
+        add_writebuffer(WBloc, WBval,  get_out_result(&alu), get_out_reg(&alu));
+      }
+
     }
+    //printf("checking if memory writback\n");
+    if (is_finished_mem(&memo)){
+      if (get_out_reg_m(&memo) >= 0){
+        add_writebuffer(WBloc, WBval, get_out_result_m(&memo), get_out_reg_m(&memo));
+      }
+
+    }
+    //printf("unit stats:\n");
+    //printf("unit stats  alu finished: %d\n", is_finished(&alu));
+    //printf("unit stats  memory finished: %d\n", is_finished_mem(&memo));
+    //printf("unit stats  brancher finished: %d\n", is_finished_branch(&branchunit));
+    if (is_finished(&alu) && is_finished_mem(&memo) && is_finished_branch(&branchunit)){
+      Execute(&execute_next, RF, &finished, &alu, &memo, &branchunit, WBloc, WBval);
+      if (!get_waiting(&fetch_u) && !get_halt(&fetch_u)){
+        instructions++;
+  		  Fetch(&fetch_current, &fetch_u);
+      }
+      else{
+        null_instr(&fetch_current);
+      }
+
+      checkifhalt(&fetch_current, &fetch_u);
+      checkifbranch(&fetch_current, &fetch_u);
+
+      if (!get_waiting(&fetch_u) && !get_halt(&fetch_u)){
+          increment_pc(&fetch_u);
+      }
+
+      decode_current = decode_next;
+  		Decode();
+      decode_next = fetch_current;
+      execute_next = decode_current;
+    }
+
+
 
     //printf("fetched instruction: %d\n" ,fetch_current.opcode);
-    checkifhalt(&fetch_current, &fetch_u);
-    checkifbranch(&fetch_current, &fetch_u);
 
-    if (!get_waiting(&fetch_u) && !get_halt(&fetch_u)){
-        increment_pc(&fetch_u);
-    }
-
-    decode_current = decode_next;
-		Decode();
     //print_instr(&fetch_current);
-
-		Execute(execute_next.opcode, execute_next.operandres, execute_next.operand1, execute_next.operand2, RF, MEM, &fetch_u, &finished);
-
-
-
-    decode_next = fetch_current;
-    execute_next = decode_current;
-
 
     cycles += 1;
 
 	}
+  /*
+  for (int i = 0; i < 32; i++){
+    printf("RF element: %d value %d\n" , i, RF[i]);
+  }
+  */
   printf("\ncycles:%d instructions:%d instructions per cycle: %3f\n", cycles, instructions, (float)instructions/(float)cycles);
 
 }
@@ -202,5 +318,11 @@ int main() {
   run_instr_set(&instruction_set);
   printf("\n--------- Finished factorial ---------\n\n");
   clear_instr(&instruction_set);
-
+/*
+  generate(4, &instruction_set);
+  printf("--------- Running factorial ---------\n\n");
+  run_instr_set(&instruction_set);
+  printf("\n--------- Finished factorial ---------\n\n");
+  clear_instr(&instruction_set);
+  */
 }
